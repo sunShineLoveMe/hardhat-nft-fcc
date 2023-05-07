d0a2bf87e2311bf496a46ba2fa49fe14974eb9b1
sunShineLoveMe <juneandrao@outlook.com>
2023-05-07 21:30:51 +0800

    test contract file -- BasicNft.sol

--- test/unit/basicNft.test.js
+++ test/unit/basicNft.test.js
@@ -24,4 +24,24 @@ const { assert } = require('chai');
                 assert.equal(tokenCounter.toString(), "0")
             })
         })
+
+        describe("Mint NFT", function() {
+            beforeEach(async() => {
+                const txResponse = await basicNft.mintNft()
+                await txResponse.wait(1)
+            })
+            it("Allows users to mint an NFT, and updates appropriately", async() => {
+                const tokenURI = await basicNft.tokenURI(0)
+                const tokenCounter = await basicNft.getTokenCounter()
+
+                assert.equal(tokenCounter.toString(), "1")
+                assert.equal(tokenURI, await basicNft.tokenURI(0))
+            })
+            it("show the correct balance and owner of an NFT", async() => {
+                const balance = await basicNft.balanceOf(deployer.address)
+                const owner = await basicNft.ownerOf(0)
+                assert.equal(balance.toString(), "1")
+                assert.equal(owner, deployer.address)
+            })
+        })
     })
